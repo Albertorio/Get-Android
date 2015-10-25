3a188c1291daf6192f691e0d548c55e3c2159e78
Luis <albertorio.10@hotmail.com>
2015-10-25 11:50:28 -0400

    adding log in and sign up activities

--- app/src/main/java/com/chamas/luis/get/MainActivity.java
+++ app/src/main/java/com/chamas/luis/get/MainActivity.java
@@ -1,9 +1,11 @@
 package com.chamas.luis.get;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -33,5 +35,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void signup(View view) {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
+        finish();
     }
 }
