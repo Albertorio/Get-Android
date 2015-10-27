ff45f4613165cca29413db47ea3b9e51c3374306
Luis <albertorio.10@hotmail.com>
2015-10-27 15:57:12 -0400

    finished login ui and added variables. Started working on set up profile after sign up.

--- app/src/main/java/com/chamas/luis/get/LogIn.java
+++ app/src/main/java/com/chamas/luis/get/LogIn.java
@@ -2,13 +2,25 @@ package com.chamas.luis.get;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LogIn extends ActionBarActivity {
+    EditText EmailOrUsername, Password;
+    TextView ForgotPass;
+    String emailOrUserStr, passStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        EmailOrUsername = (EditText)findViewById(R.id.SignInEmailOrUsernameEditText);
+        Password = (EditText)findViewById(R.id.SignInPasswordEditText);
+        ForgotPass = (TextView)findViewById(R.id.SignInForgotPasswordTextView);
+
+        emailOrUserStr = EmailOrUsername.getText().toString();
+        passStr = Password.getText().toString();
     }
 
 }
---
+++ app/src/main/java/com/chamas/luis/get/SetUpProfile.java
@@ -0,0 +1,14 @@
+package com.chamas.luis.get;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SetUpProfile extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up_profile);
+    }
+
+}
