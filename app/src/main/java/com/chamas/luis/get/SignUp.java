7d8f0ed5dc8f183240be833604385e0a5c9e1d9a
Luis <albertorio.10@hotmail.com>
2015-10-26 22:14:51 -0400

    made a basic sign up ui and declared variables that will be used in the db

--- app/src/main/java/com/chamas/luis/get/SignUp.java
+++ app/src/main/java/com/chamas/luis/get/SignUp.java
@@ -2,13 +2,30 @@ package com.chamas.luis.get;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp extends ActionBarActivity {
+    private EditText Username, Email, password, ConfirmPass;
+    private Button SignupButton;
+    private String usernameStr, emailStr, passwordStr, confirmPassStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Username = (EditText)findViewById(R.id.SignUpUsernameEditText);
+        Email = (EditText)findViewById(R.id.SignUpEmailEditText);
+        password = (EditText)findViewById(R.id.SignUpPasswordEditText);
+        ConfirmPass = (EditText)findViewById(R.id.SignUpConfirmPasswordEditText);
+        SignupButton = (Button)findViewById(R.id.SignUpButton);
+
+        usernameStr = Username.getText().toString();
+        emailStr = Email.getText().toString();
+        passwordStr = password.getText().toString();
+        confirmPassStr = ConfirmPass.getText().toString();
+
     }
 
 }
